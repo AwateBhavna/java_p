08da5277b5798ae3953d02f3159752ad2dd848c0
AwateBhavna <awatebhavana.sdet@gmail>
2023-01-04 10:38:07 +0530

    java

---
+++ Naveen_atomation_Java/src/Java_Training/IfElseConcept.java
@@ -0,0 +1,42 @@
+package Java_Training;
+
+public class IfElseConcept {
+
+	public static void main(String[] args) {
+		int a =30;
+		int b=20;
+		if(b>a) {
+			System.out.println("b is greater then a");
+		}
+		else {
+			System.out.println("a is greater than b");
+		}
+// comparision operators
+		// < > <= >= !=
+		int c =40;
+		int d =40;
+		 if (c==d) {
+			 System.out.println("c and d are equal");
+			 
+		 }else {
+			 System.out.println("C and d are not equal");
+		 }
+		 
+		 // write a logic to find out the highest number
+		 int a1 =400;
+		 int b1= 500;
+		 int c1=300;
+		 
+		 // nested if=else
+		 if(a1>b1 & a1>c1) {
+			 System.out.println("a1 is the greatest");
+		 }
+		 else if(b1>c1) {
+			 System.out.println("b1 is the greatest");
+		 }
+		 else {
+			 System.out.println("c1 is the greatest");
+		 }
+	}
+
+}
---
+++ Naveen_atomation_Java/src/Java_Training/String_concat.java
@@ -0,0 +1,36 @@
+package Java_Training;
+
+public class String_concat {
+
+	public static void main(String[] args) {
+	
+	int a= 100;
+	int b =200;
+	
+	String x = "Hello";
+	
+	String y = "World";
+	
+	double c= 12.33;
+	double d = 10.33;
+	System.out.println(a+b);
+	System.out.println(x+y);
+	System.out.println(a+b+x+y);
+	System.out.println(x+y+(a+b));
+	System.out.println(a+b+x+y+x+b+y);
+	System.out.println(a+b+x+y+a+b);
+	System.out.println(c+d);
+	System.out.println(x+y+c+d);
+	System.out.println("Hellowrold");
+System.out.println(a);
+System.out.println("the value of a is:"+a);
+System.out.println("the value of a is:"+b);
+System.out.println("the addition  of a and b is:"+(a+b));
+// ln is new space line new of ln means
+System.out.print("Hello Testing ");
+System.out.println("Hello Selenium");
+
+	
+	}
+
+}
